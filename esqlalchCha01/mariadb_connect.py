b3228969d89f7f4020ebdbc90c769502df4fef54
Gabriel Perdue <gnperdue@me.com>
2016-02-14 13:30:13 -0600

    fix dumb typo

--- esqlalchCha01/mariadb_connect.py
+++ esqlalchCha01/mariadb_connect.py
@@ -4,9 +4,9 @@ docdir = '/Documents/ProgrammingTokens/MariaDB/'
 username_file = os.environ['HOME'] + docdir + 'my_username.txt'
 passwd_file = os.environ['HOME'] + docdir + 'my_passwd.txt'
 with open(username_file, 'r') as f:
-    username = f.readline.strip()
+    username = f.readline().strip()
 with open(passwd_file, 'r') as f:
-    passwd = f.readline.strip()
+    passwd = f.readline().strip()
 
 conn_str = 'mysql+pymysql://' + username + ':' + passwd + \
            '@localhost/essential_alchemy'
