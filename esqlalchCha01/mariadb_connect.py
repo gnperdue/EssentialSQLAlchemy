bc9b65e28100c8fc8326b287cc338ce8d6d0a7c8
Gabriel Perdue <gnperdue@me.com>
2016-02-14 13:29:09 -0600

    connection info for mariadb

---
+++ esqlalchCha01/mariadb_connect.py
@@ -0,0 +1,12 @@
+import os
+
+docdir = '/Documents/ProgrammingTokens/MariaDB/'
+username_file = os.environ['HOME'] + docdir + 'my_username.txt'
+passwd_file = os.environ['HOME'] + docdir + 'my_passwd.txt'
+with open(username_file, 'r') as f:
+    username = f.readline.strip()
+with open(passwd_file, 'r') as f:
+    passwd = f.readline.strip()
+
+conn_str = 'mysql+pymysql://' + username + ':' + passwd + \
+           '@localhost/essential_alchemy'
